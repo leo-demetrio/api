0658ab5e6f853aba40b3f9d151a175ba0e56139e
leo-demetrio <leocdemetrio@gamil.com>
2023-03-01 20:10:12 -0300

    Rota pessoa

---
+++ src/main/java/br/com/projeto/api/ControleController.java
@@ -0,0 +1,16 @@
+package br.com.projeto.api;
+
+import br.com.projeto.api.model.Pessoa;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControleController {
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p){
+        return p;
+    }
+}
