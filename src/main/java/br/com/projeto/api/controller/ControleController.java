32a1b5e9c63110be0307571d45ad2bc823d7cdcd
leo-demetrio <leocdemetrio@gamil.com>
2023-03-06 18:17:10 -0300

    Find all

--- src/main/java/br/com/projeto/api/controller/ControleController.java
+++ src/main/java/br/com/projeto/api/controller/ControleController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ControleController {
 
     @Autowired
     private PessoaRepository acao;
+
+    @GetMapping("pessoa/findAll")
+    public List<Pessoa> findAll(){
+        return acao.findAll();
+    }
 
     @PostMapping("/pessoa/cadastrar")
     public Pessoa cadastrar(@RequestBody Pessoa p){
--- src/main/java/br/com/projeto/api/repository/PessoaRepository.java
+++ src/main/java/br/com/projeto/api/repository/PessoaRepository.java
@@ -4,6 +4,10 @@ import br.com.projeto.api.model.Pessoa;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PessoaRepository extends CrudRepository<Pessoa, Integer> {
+
+    List<Pessoa> findAll();
 }
